160bf4c68e4938a4211e4103810677badcb10455
gitbugd20p <155899250+gitbugd20p@users.noreply.github.com>
2026-07-03 18:23:44 +0600

    feat: added proper utils function for best practice
    - added catch async for generic controller call functions
    - added sendResponse function for generic response

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,30 +1,21 @@
 import { Request, Response, Router } from "express";
 import httpStatus from "http-status";
 import { userService } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 
-const registerUser = async (req: Request, res: Response) => {
-    try {
-        const payload = req.body;
+const registerUser = catchAsync(async (req: Request, res: Response) => {
+    const payload = req.body;
 
-        const user = await userService.registerUserIntoDB(payload);
+    const user = await userService.registerUserIntoDB(payload);
 
-        res.status(httpStatus.CREATED).json({
-            success: true,
-            statusCode: httpStatus.CREATED,
-            message: "User registered successfully!",
-            data: { user },
-        });
-    } catch (error) {
-        console.log(error);
-
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-            success: false,
-            statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-            message: "Failed to register user",
-            error: (error as Error).message,
-        });
-    }
-};
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.CREATED,
+        message: "User registered successfully!",
+        data: { user },
+    });
+});
 
 export const userController = {
     registerUser,
