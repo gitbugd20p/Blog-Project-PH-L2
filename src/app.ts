46ab448f27b06ad6cf59bb11dbb81c3fcd045677
gitbugd20p <155899250+gitbugd20p@users.noreply.github.com>
2026-07-04 00:46:13 +0600

    feat: implemented accessToken and refreshToken in auth module

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import config from "./config";
 import { userRoutes } from "./modules/user/user.route";
+import { authRoutes } from "./modules/auth/auth.route";
 
 const app: Application = express();
 
@@ -22,5 +23,6 @@ app.get("/", async (req: Request, res: Response) => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 export default app;
